ec167daabff38aa81c3b5f676dc867d7fc6bb5ef
LIGHT131313 <131313light@gmail.com>
2023-10-25 08:04:15 +0300

    v1

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,15 +1,14 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+type TypeComp = { title: string };
 
-class Component {
-  constructor (public props:T) {
-
-  }
+class Component<T extends TypeComp> {
+  constructor(public props: T) {}
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<TypeComp> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
